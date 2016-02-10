745a4c114b5ef36027e056e1957b4d73f31f0cba
Charlie Volpe <volpegames@gmail.com>
2016-02-10 07:54:05 -0800

    Added an entity class to have a base for objects.

---
+++ js/entity.js
@@ -0,0 +1,22 @@
+/* entity.js
+*  
+*  Author: Volpe 2016
+*  
+*  Description:
+*  Object oriented programming starter (oops).
+*/
+
+// Create basic Entity
+var Entity = function(name) {
+	this.name = name;
+	console.log("Entity instatiated.");
+}
+
+// Give Entity a print name function
+Entity.prototype.printName = function() {
+	console.log(this.name);
+}
+
+// Test code
+var entity1 = new Entity('entity1');
+entity1.printName();
