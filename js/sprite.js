107cb73fef187394aafe62fa41c22a9f35e85d94
Charlie Volpe <volpegames@gmail.com>
2016-02-11 08:07:58 -0800

    WIP: started spriteAnim functions

--- js/sprite.js
+++ js/sprite.js
@@ -17,6 +17,7 @@ var Sprite = function(name, url, iunitWidth, iunitHeight, irows, icols) {
   this.icols = icols;
 }
 
+// Create Sprite Animation Object
 var SpriteAnim = function(name, sprite, bloop, istart, iend, ispeed) {
   this.name = name;
   this.sprite = sprite;
@@ -24,10 +25,40 @@ var SpriteAnim = function(name, sprite, bloop, istart, iend, ispeed) {
   this.istart = istart;
   this.iend = iend;
   this.ispeed = ispeed;
+
+  this.bisPlaying = false;
 }
 
+// Play through the animation once
 SpriteAnim.prototype.Play = function() {
-  for(var i = this.istart; i<this.iend; i++) {
-
+  this.bisPlaying = true;
+  for(var i = this.istart; i<=this.iend; i++) {
+    if( this.bisPlaying ) {
+      console.log(i);
+    }
   }
+  this.bisPlaying = false;
 }
+
+// Play through the animation once in reverse
+SpriteAnim.prototype.Reverse = function() {
+  this.bisPlaying = true;
+  for(var i = this.iend; i>=this.istart; i--) {
+    if( this.bisPlaying ) {
+      console.log(i);
+    }
+  }
+  this.bisPlaying = false;
+}
+
+// Stop animation
+SpriteAnim.prototype.Stop = function() {
+  this.bisPlaying = false;
+}
+
+// Test sprite code
+var sprite = new Sprite("test","fake",32,32,16,16);
+var spriteAnim = new SpriteAnim("testAnim", sprite, false, 1, 30, 30);
+
+spriteAnim.Play();
+spriteAnim.Reverse();
