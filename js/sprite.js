001e241296505a8ec6db089174f3fdfbad42d090
charlie_volpe <volpegames@gmail.com>
2016-02-11 21:19:24 +0000

    Clear out sprite code. Want to rethink it.

--- js/sprite.js
+++ js/sprite.js
@@ -31,34 +31,15 @@ var SpriteAnim = function(name, sprite, bloop, istart, iend, ispeed) {
 
 // Play through the animation once
 SpriteAnim.prototype.Play = function() {
-  this.bisPlaying = true;
-  for(var i = this.istart; i<=this.iend; i++) {
-    if( this.bisPlaying ) {
-      console.log(i);
-    }
-  }
-  this.bisPlaying = false;
+  
 }
 
 // Play through the animation once in reverse
 SpriteAnim.prototype.Reverse = function() {
-  this.bisPlaying = true;
-  for(var i = this.iend; i>=this.istart; i--) {
-    if( this.bisPlaying ) {
-      console.log(i);
-    }
-  }
-  this.bisPlaying = false;
+  
 }
 
 // Stop animation
 SpriteAnim.prototype.Stop = function() {
-  this.bisPlaying = false;
+  
 }
-
-// Test sprite code
-var sprite = new Sprite("test","fake",32,32,16,16);
-var spriteAnim = new SpriteAnim("testAnim", sprite, false, 1, 30, 30);
-
-spriteAnim.Play();
-spriteAnim.Reverse();
